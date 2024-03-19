530cddc741067b645b6edf2bbb4701d6b5608a54
Vadim Rutkovsky <vrutkovs@redhat.com>
2024-03-19 16:31:36 +0100

    server: mount model as read-only

--- server/src/k8s_job.rs
+++ server/src/k8s_job.rs
@@ -180,6 +180,7 @@ pub fn create_job_for_prompt(
                             "name": "model",
                             "mountPath": "/opt/app-root/src/model",
                             "subPath": "model",
+                            "readOnly": "true",
                         }]
                     }],
                     "volumes": [{
